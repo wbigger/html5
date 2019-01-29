c8f5b46ab02aae3b9a58c87e2cbec72a92266ccd
Claudio Capobianco <claudio.capobianco@istruzione.it>
2019-01-29 11:46:09 +0100

    Add get call with jquery

--- code/app.js
+++ code/app.js
@@ -2,6 +2,19 @@ app = {
     init : function() {
         console.log("init inside app!");
         $("title").text("New title");
+        $.get("data.json")
+        .done(app.onSuccess)
+        .fail(app.onError);
+        
+    },
+    onSuccess:function(jsonData){
+        // processa i dati qui!!!
+        console.log(jsonData);
+    },
+    
+    onError:function(e){
+        console.log("error!");
+        console.log(JSON.stringify(e));
     }
 };
 
